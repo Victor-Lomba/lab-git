eacf08e06c77617a06f3c83b90cad94c9c0c962b
João Fernandez <joao.jfpfernandez@gmail.com>
2021-05-04 13:07:54 -0300

    Criei uma função para concatenar string

--- index.cpp
+++ index.cpp
@@ -1,4 +1,6 @@
 #include<iostream>
+#include <conio.h>
+#include <string.h>
 
 using namespace std;
 
@@ -14,8 +16,27 @@ int FibIndex(int index){
 	return n1;
 }
 
+void meu_strcat (char v1[], char v2[])
+{
+string s1, s2;
+s1 = v1;
+s2 = v2;
+s1 = s1+s2;
+strcpy(v1, s1.c_str());
+}
 
 main(){
 	//teste fib
 	cout << FibIndex(8);//deve retornar 21
+	
+	
+	char V1[256], V2[256];
+	string S1, S2;
+	S1 = "\nCEFET";
+	S2 = "/RJ";
+	strcpy(V1, S1.c_str());
+	strcpy(V2, S2.c_str());
+	meu_strcat(V1, V2);
+	S1 = V1;
+	cout << S1;
 }
